6d4269aac12d9fb2c3eed8a5707cd4b7b07c71f3
EshanW313 <EshanW.313@users.noreply.github.com>
2019-12-10 20:26:04 -0800

    Added main menu

---
+++ Mochi/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string previousGameFile = "";
+
+    public void StartNewGame()
+    {
+        string filePath = Application.dataPath + "/Resources/gameFiles";
+        UnityEngine.Windows.File.Delete(filePath + "/0.txt");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Chapter0");
+    }
+
+    public void LoadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Chapter0");
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
